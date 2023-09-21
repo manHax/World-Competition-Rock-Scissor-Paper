8ca1f4c92b473df14842a1264fa0162a5997bf17
L Hakim <luqmanul.hakim@dwidasa.com>
2023-09-21 10:20:42 +0700

    feat : nambah fitur baru yeah

---
+++ src/testBranching/test.java
@@ -0,0 +1,13 @@
+package testBranching;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lhaki
+ * Date: 9/21/2023
+ * Time: 10:19 AM
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this class for test branch");
+    }
+}
